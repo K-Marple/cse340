96475db7e5e1cbcd161d99b91aa2a719755e26c9
K-Marple <taeshinoberu@outlook.com>
2025-02-14 19:54:32 -0800

    registration linked to database

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -8,4 +8,7 @@ router.get('/login', utilities.handleErrors(accController.buildLogin))
 
 router.get('/registration', utilities.handleErrors(accController.buildRegistration))
 
+// Post router
+router.post('/register', utilities.handleErrors(accController.registerAccount))
+
 module.exports = router;
