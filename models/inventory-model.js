8c761231cf475a21d6f294e31f39b6267c1ed833
K-Marple <taeshinoberu@outlook.com>
2025-03-01 18:39:52 -0800

    insert inventory

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -52,6 +52,21 @@ async function addClassification(classification_name) {
   }
 }
 
+/* ***************************
+ * Add inventory to database
+ * ************************** */
+async function addInventory(classification_id, inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color) {
+  try {
+    const sql = `INSERT INTO inventory 
+      (classification_id, inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color) 
+      VALUES ($1 $2 $3 $4 $5 $6 $7 $8 $9 $10) 
+      RETURNING *`
+    return await pool.query(sql, [classification_id, inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color])
+  } catch (error) {
+      return error.message
+  }
+}
+
 module.exports = {getClassifications, 
   getInventoryByClassificationId, getInventoryByInventoryId,
-  addClassification};
+  addClassification, addInventory};
