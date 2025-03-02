1c242825ebd0479e26f87da55911fdd1a82454b2
K-Marple <taeshinoberu@outlook.com>
2025-03-01 22:51:50 -0800

    add inventory page edits

--- controllers/invController.js
+++ controllers/invController.js
@@ -64,6 +64,7 @@ invCont.buildAddClassification = async function(req, res, next) {
  * ******************** */
 invCont.buildAddInventory = async function(req, res, next) {
     let nav = await utilities.getNav()
+    const classificationList = await utilities.buildClassificationList()
     res.render("./inventory/addInventory", {
         title: 'Add Inventory',
         nav,
